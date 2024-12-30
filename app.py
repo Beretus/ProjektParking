54575c4c34da9d36eb1703564d723c9b8dc2eabc
Beretus <48188253+Beretus@users.noreply.github.com>
2024-12-30 01:15:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -302,7 +302,8 @@ def status():
 @api_or_login_required
 def generate_qr(current_user):
     username = current_user.username  
-    qr_data = f"{username}-{uuid.uuid4()}"  
+    qr_data = f"{username}-{uuid.uuid4()}"
+    qr = qrcode.make(qr_data)
     print(f"Generated QR data: {qr_data}")  # Debug: wyświetlenie qr_data w konsoli
     qr_io = io.BytesIO()
     qr.save(qr_io, 'PNG')
